e2dc1e82202445a1469e854517c3ffb697b18058
Chris Calascione <chris.calascione@gmail.com>
2022-02-22 00:10:39 -0500

    remove initial boilerplate and add fakerjs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import BirthdayFinder from './BirthdayFinder';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => (
+    <div id="app" className="app">
+        <div className="app__header">
+            <h1>Filter employees by their birthday month</h1>
+            <strong>App Requirements</strong>
+            <ul>
+                <li>Given I have an array of employee records that include their birthday</li>
+                <li>When I execute this script/program with the array of employees</li>
+                <li>Then I am returned an array of employees who's birthday is in the current month</li>
+            </ul>
+            <strong>App Instructions</strong>
+            <ol>
+                <li>
+                    Add an Array of records to the User Input field. The array can be typed directly into the field, uploaded from a JSON file, or fake data can be used by clicking the Create Fake Data Button.
+                </li>
+                <li>
+                    By default, the current month is selected to filter by. You may use the "Month to filter by" select to change the month.
+                </li>
+                <li>
+                    Click the "Filter Data" button to return the filtered list of records.
+                </li>
+            </ol>
+        </div>
+      
+    <BirthdayFinder />
     </div>
-  );
-}
+);
 
 export default App;
